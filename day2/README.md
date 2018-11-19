b25b42d6d8a27365369e29a24fa085d1947ffe8e
Senal Kumarage <senalkumarage@gmail.com>
2018-11-19 10:45:38 +0800

    Adding day 2

---
+++ day2/README.md
@@ -0,0 +1,15 @@
+## Data Types
+
+### Strings
+
+can use single, double, triple qoutes
+
+### Integers
+
+- Use two * for exponents
+- 3 ** 2 => 9
+
+### Floats
+
+- In python2 3/2 will output 1
+- It ommits the remainder so if you want 1.5 you need to do this 3.0/2 or 3/2.0
---
+++ day2/variables.py
@@ -0,0 +1,37 @@
+message = 'Hello Python World...!'
+print (message)
+
+message = 'Message changed...'
+
+print(message)
+
+name = 'test case'
+
+print(name.title())
+
+print(name.upper())
+
+print(name.upper().lower())
+
+# \n newline
+# \t tab
+
+new_language = ' python '
+
+print(new_language.rstrip())    # strips spaces from right end
+
+print(new_language.lstrip())    # strips spaces from left end
+
+print(new_language.strip())     # strips spaces from both ends
+
+print(1 + 2)
+
+print(1 + 3.0)
+
+print(3 ** 2)
+
+print(2 + 4*4)
+
+print(3/2)
+
+print(3/2.0)
